dd1be7f8796654d075a0d484b433c4d8c2dbe86f
João Junior <joaospj.dev@gmail.com>
2020-06-16 21:11:13 -0300

    Timer with Web Worker

--- app.js
+++ app.js
@@ -1,78 +1,37 @@
-class Timer {
-  constructor(duration, start, pause, reset, timerInput) {
-    this.duration = duration;
-    this.start = start;
-    this.pause = pause;
-    this.reset = reset;
-    this.timerInput = timerInput;
+const duration = document.querySelector("#duration");
+const timerInput = document.querySelector("#timerInput");
+const start = document.querySelector("#start");
+const pause = document.querySelector("#pause");
+const reset = document.querySelector("#reset");
 
-    this.start.addEventListener("click", this.startTimer);
-    this.pause.addEventListener("click", this.pauseTimer);
-    this.reset.addEventListener("click", this.resetTimer);
-  }
+if (window.Worker) {
+  const myWorker = new Worker("timer.js");
 
-  startTimer = () => {
-    // Check if input is greater than 0 to start,
-    // and prevent Start button action when resumed
-    if (!this.interval && parseFloat(this.timerInput.value)) {
-      // Allow resume time when paused
-      if (!this.timerInput.disabled) {
-        this.seconds = parseFloat(this.timerInput.value);
-        // Set the correct type of the number to timerInput
-        this.timerInput.value = this.seconds;
-        this.timerInput.disabled = true;
-      }
-      this.interval = setInterval(this.tick, 10);
+  start.onclick = function () {
+    console.log(timerInput.value);
+
+    myWorker.postMessage(["start", timerInput.value]);
+    if (parseFloat(timerInput.value)) {
+      timerInput.disabled = true;
     }
   };
 
-  pauseTimer = () => {
-    clearInterval(this.interval);
-    // Doing this the timer can be paused and resumed anytime
-    this.interval = undefined;
+  pause.onclick = function () {
+    myWorker.postMessage(["pause"]);
   };
 
-  resetTimer = (fromTick) => {
-    // When reset is called for tick(), keep the duration value at 0.00
-    if (fromTick !== 1) {
-      this.timeLeft = parseFloat(this.timerInput.value) || 0;
-    }
-    this.pauseTimer();
-    // timerInput must be disabled to prevent change a timer in course
-    this.timerInput.disabled = false;
+  reset.onclick = function () {
+    myWorker.postMessage(["reset"]);
+    timerInput.disabled = false;
   };
 
-  tick = () => {
-    if (this.timeLeft <= 0) {
-      // Fix a math bug
-      this.timeLeft = 0;
-
-      // Reset timer when timeLeft reaches to 0.00
-      this.resetTimer(1);
+  myWorker.onmessage = function (e) {
+    if (e.data === "reset") {
+      timerInput.disabled = false;
     } else {
-      this.timeLeft = this.timeLeft - 0.01;
+      duration.innerText = e.data;
     }
   };
-
-  get timeLeft() {
-    return parseFloat(this.seconds);
-  }
-
-  set timeLeft(time) {
-    this.seconds = time;
-    this.duration.innerText = this.formatSeconds(this.seconds);
-  }
-
-  formatSeconds = (seconds) => {
-    const sec = (seconds % 60).toFixed(2);
-    return sec < 10 ? `0${sec}` : sec;
-  };
+} else {
+  console.log("Your browser doesn't support web workers.");
 }
-
-const duration = document.getElementById("duration");
-const timerInput = document.getElementById("timerInput");
-const start = document.getElementById("start");
-const pause = document.getElementById("pause");
-const reset = document.getElementById("reset");
-
-const timer = new Timer(duration, start, pause, reset, timerInput);
---
+++ timer.js
@@ -0,0 +1,78 @@
+class Timer {
+  constructor() {
+    this.disabled = false;
+  }
+
+  startTimer = (timerInput) => {
+    this.timerInput = timerInput;
+    if (!this.interval && parseFloat(this.timerInput)) {
+      if (!this.disabled) {
+        this.seconds = parseFloat(this.timerInput);
+        this.disabled = true;
+      }
+      this.interval = setInterval(this.tick, 10);
+    }
+  };
+
+  pauseTimer = () => {
+    clearInterval(this.interval);
+    // Doing this the timer can be paused and resumed anytime
+    this.interval = 0;
+  };
+
+  resetTimer = (fromTick) => {
+    // When reset is called for tick(), keep the duration value at 0.00
+    if (fromTick !== 1) {
+      this.timeLeft = parseFloat(this.timerInput) || 0;
+    }
+    this.pauseTimer();
+    // timerInput must be disabled to prevent change a timer in course
+    this.disabled = false;
+    postMessage("reset");
+  };
+
+  tick = () => {
+    if (this.timeLeft <= 0) {
+      // Fix a math bug
+      this.timeLeft = 0;
+
+      // Reset timer when timeLeft reaches to 0.00
+      this.resetTimer(1);
+    } else {
+      this.timeLeft = this.timeLeft - 0.01;
+    }
+  };
+
+  get timeLeft() {
+    return parseFloat(this.seconds);
+  }
+
+  set timeLeft(time) {
+    this.seconds = time;
+    postMessage(this.formatSeconds(this.seconds));
+  }
+
+  formatSeconds = (seconds) => {
+    const sec = (seconds % 60).toFixed(2);
+    return sec < 10 ? `0${sec}` : sec;
+  };
+}
+
+const timer = new Timer();
+
+onmessage = function (e) {
+  switch (e.data[0]) {
+    case "start":
+      timer.startTimer(e.data[1]);
+      break;
+    case "pause":
+      timer.pauseTimer();
+      break;
+    case "reset":
+      timer.resetTimer();
+      break;
+    default:
+      postMessage("wrong case!");
+      break;
+  }
+};
